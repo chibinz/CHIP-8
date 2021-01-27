60ff7da2aa0f278b6336e7223e13bf1e2c28bf85
Chibin Zhang <chibinzhang@hotmail.com>
2021-01-27 15:34:20 +0800

    Implement fb_draw_sprite

--- graphics.c
+++ graphics.c
@@ -4,4 +4,17 @@
 
 void fb_clear(u8 *fb) { memset(fb, 0, 32 * 64); }
 
-void fb_draw_sprite(u8 *fb, u8 *sprite) {}
+u8 fb_draw_sprite(u8 *fb, u8 *sprite, u8 vx, u8 vy, u8 n) {
+  u8 overflow = 0;
+  u8 pixel = 0;
+
+  for (usize y = vy; y < vy + n; y++) {
+    for (usize x = 0; x < 8; x++) {
+      pixel = sprite[y] & (1 << (7 - x)) ? 0xff : 0;
+      overflow |= pixel & fb[y * 64 + vx + x];
+      fb[y * 64 + vx + x] ^= pixel;
+    }
+  }
+
+  return overflow == 0xff;
+}
